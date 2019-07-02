04173e87e302e9506418c8118629bb642cf7a934
Agnieszka Rakowska <ar21@o2.pl>
2019-07-02 12:27:54 +0200

    create handleChange & handleSearch methods

--- src/components/Img/Img.js
+++ src/components/Img/Img.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Img = props => (
     <li>
+    
         <a target="_blank" href={props.url} >
             <img src={props.src} alt={props.alt}/>
         </a>
+        <a href={props.download} download={props.download}>download</a>
+        <a href="https://www.antennahouse.com/XSLsample/pdf/sample-link_1.pdf" download>Download Form</a> 
     </li>
 )
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,11 +5,12 @@ import Img from '../Img/Img';
 
 class SearchBar extends React.Component {
     state = {
+        term: 'sun',
         images: []
     }
 
     componentDidMount() {
-        fetch('https://pixabay.com/api/?key=7129137-0ebf8cbfe5e38668049f26d2b&q=yellow+flowers&image_type=photo')
+        fetch(`https://pixabay.com/api/?key=7129137-0ebf8cbfe5e38668049f26d2b&q=${this.state.term}&image_type=photo`)
         .then(response => response.json() )
         .then(json => {
             this.setState({
@@ -18,19 +19,37 @@ class SearchBar extends React.Component {
         } )
     }
 
-    
+handleChange = (e) => {
+    e.preventDefault();
+
+    this.setState({ 
+        term: e.target.value
+    });
+}
+
+handleSearch = (e) => {
+    e.preventDefault();
+
+    fetch(`https://pixabay.com/api/?key=7129137-0ebf8cbfe5e38668049f26d2b&q=${this.state.term}&image_type=photo`)
+    .then(response => response.json() )
+    .then(json => {
+        this.setState({
+            images: json.hits
+        })
+    } )
+}
 
     render(){
         const images = this.state.images.map(img => (
-            <Img src={img.previewURL} url={img.largeImageURL}/>
+            <Img src={img.previewURL} url={img.largeImageURL} download={img.largeImageURL}/>
         ))
 
         return (
             <>
                 <form action="" method="get">
                         <label htmlFor="name">Find images: </label>
-                        <input type="text" name="name" id="name" placeholder="Search images..."  required />
-                        <input type="submit" value="Search" />
+                        <input onChange={this.handleChange} type="text" name="name" id="name" placeholder="Search images..."  required />
+                        <input onClick={this.handleSearch} type="submit" value="Search"/>
                 </form>
                 <ul>
                     {images}
