1e9f1734666140dfbc5d219ea3c16437d3364eb9
Agnieszka Rakowska <ar21@o2.pl>
2019-07-17 10:52:37 +0200

    code refactoring

--- src/components/FontFinder/FontFinder.js
+++ src/components/FontFinder/FontFinder.js
@@ -17,222 +17,186 @@ const StyledScroll = styled(InfiniteScroll)`
 
 class FontFinder extends React.Component {
     state = {
-        allFonts: [],
+        all: [],
         allSerif: [],
         allSansSerif: [],
-        fonts: [],
-        serif: [],
-        sansSerif: [],
-
+        selectedToDisplay: [],
+        visibleFonts: [],
+        visibleSerif: [],
+        visibleSansSerif: [],
         randomFont: [],
+        categorySelected: [],
         fontFamilies: [],
-
         fontsAmount: null,
         results: 1,
-
-
-        showLoader: false,
-     
-        test: [],
-        categorySelected: [],
         hasMore: false
     }
 
-
-
-componentDidMount() {
-    fetch(`https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDR7baDJT2PMQ-FuyAZHCYNQS93yy6n2Ms`)
-    .then(response => {
-        if(response.ok){
-            return response;
-        }
-    })
-    .then(response => response.json() )
-    .then(json => {
-        const allFonts = json.items;
-        const fonts = allFonts.filter(item => item.category === "serif" || item.category === "sans-serif" ? item : null);
-        const randomFont = [fonts[Math.floor(Math.random()*fonts.length)]];
-
-        this.setState({
-            test: fonts,
-            allFonts: fonts,
-            allSerif: fonts.filter(item => item.category === "serif" ? item : null),
-            allSansSerif: fonts.filter(item => item.category === "sans-serif" ? item : null),
-            fonts: this.state.allFonts.slice(0, this.state.results),
-            serif: this.state.allSerif.slice(0, this.state.results),
-            sansSerif: this.state.allSansSerif.slice(0, this.state.results),
-            randomFont: randomFont,
-            categorySelected: randomFont,
-            fontFamilies: fonts.map(font => ( 
-                
-                <link href={`https://fonts.googleapis.com/css?family=${font.family.split(' ').join('+')}&display=swap`} rel="stylesheet"></link>
-               // style={{@import url(`https://fonts.googleapis.com/css?family=${font.family}`)}}
-    
-                 )),
-                 fontsAmount: fonts.length,
-               //  isLoaded: true
-
-                
-        })
-    } )
-    .catch(error => this.setState({ error: `An error occured` }))
-}
-
-
-
-showSerif = (e) => {
-    e.preventDefault(e);
-
-    this.setState({ 
-        fonts: this.state.allFonts.slice(0, 0),
-        hasMore: true,
-        allFonts: this.state.allSerif,
-        categorySelected: this.state.serif,
-        results: 0,
-        //showLoader: true,
-    })
-
-}
-
-showSansSerif = (e) => {
-    e.preventDefault(e);
-
-    this.setState({ 
-        fonts: this.state.allFonts.slice(0, 0),
-        hasMore: true,
-        allFonts: this.state.allSansSerif,
-        categorySelected: this.state.sansSerif,
-        results: 0,
-        //showLoader: true,
-    })
-
-}
-
-showAll = (e) => {
-    e.preventDefault(e);
-
-    this.setState({ 
-        fonts: this.state.allFonts.slice(0, 0),
-        hasMore: true,
-        allFonts: this.state.test,
-        categorySelected: this.state.test,
-        results: 0,
-        
-        //showLoader: true,
-    })
-}
-
-
-showRandom = (e) => {
-    e.preventDefault(e);
-
-    this.setState({ 
-        fonts: this.state.allFonts.slice(0, 0),
-        hasMore: true, 
-        allFonts: this.state.randomFont,
-        categorySelected: this.state.randomFont,
-        results: 0,
-        
-        //showLoader: true,
-    })
-}
-
-fetchMoreData = () => {
-
-   
-        if (this.state.fonts.length === this.state.allFonts.length) {
-          this.setState({ hasMore: false });
-          return;
-        }
-
-    setTimeout(() => {
-      this.setState(prevState =>({
-        //  selectItems: this.state.items.slice(0,6),
-        results: prevState.results + 2,
-        fonts:  this.state.allFonts.slice(0, prevState.results + 3),
-        
-      }));
-    }, 500);
-};
-
-
-
-render(){
-    const {error, test, showLoader, categorySelected, fonts, serif, sansSerif, fontFamilies, randomFont, fontsAmount} = this.state;
-    
-
-    return (
-        <>
-            {        
-                fontFamilies
+    componentDidMount() {
+        fetch(`https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDR7baDJT2PMQ-FuyAZHCYNQS93yy6n2Ms`)
+        .then(response => {
+            if(response.ok){
+                return response;
             }
-            <div>
-                {  fontsAmount === null ?  <Loader /> : <Info info={fontsAmount} /> }
-            </div>
-            <p>{error}</p>
+        })
+        .then(response => response.json() )
+        .then(json => {
+            const allItems = json.items;
+            const fonts = allItems.filter(item => item.category === "serif" || item.category === "sans-serif" ? item : null);
+            const randomFont = [fonts[Math.floor(Math.random()*fonts.length)]];
 
-{ 
-    fontsAmount !== null ? 
-    
-    <div> 
+            this.setState({
+                all: fonts,
+                allSerif: fonts.filter(item => item.category === "serif" ? item : null),
+                allSansSerif: fonts.filter(item => item.category === "sans-serif" ? item : null),
+                selectedToDisplay: fonts,
+                visibleFonts: this.state.selectedToDisplay.slice(0, this.state.results),
+                visibleSerif: this.state.allSerif.slice(0, this.state.results),
+                visibleSansSerif: this.state.allSansSerif.slice(0, this.state.results),
+                randomFont: randomFont,
+                categorySelected: randomFont,
+                fontFamilies: fonts.map(font => ( 
+                        <link href={`https://fonts.googleapis.com/css?family=${font.family.split(' ').join('+')}&display=swap`} rel="stylesheet"></link>
+                    )),
+                fontsAmount: fonts.length,       
+            })
+        } )
+        .catch(error => this.setState({ error: `An error occured` }))
+    }
 
- 
 
-<Button category={ categorySelected } whatToShow={randomFont} handleClickFn={this.showRandom}>random font</Button>
-    <Button category={ categorySelected } whatToShow={test} handleClickFn={this.showAll}>all fonts</Button>
-       <Button category={ categorySelected } whatToShow={serif} handleClickFn={this.showSerif}>serif only</Button>
-       <Button category={ categorySelected } whatToShow={sansSerif} handleClickFn={this.showSansSerif}>sans-serif only</Button>
+    showSerif = (e) => {
+        e.preventDefault(e);
 
-        </div>
-        :
-        null 
-}
-         
+        this.setState({ 
+            visibleFonts: this.state.selectedToDisplay.slice(0, 0),
+            results: 0,
+            hasMore: true,
+            selectedToDisplay: this.state.allSerif,
+            categorySelected: this.state.visibleSerif,
+        })
+    }
 
-        <div>
- 
-   <div>
-    {showLoader ? <Loader /> : null}
-</div>
+    showSansSerif = (e) => {
+        e.preventDefault(e);
 
-{
-    categorySelected === randomFont ?   randomFont.map(font => (
-             <Font
-                 key={font.family}
-                 fontFamily={font.family}
-                 category={font.category}
+        this.setState({ 
+            visibleFonts: this.state.selectedToDisplay.slice(0, 0),
+            results: 0,
+            hasMore: true,
+            selectedToDisplay: this.state.allSansSerif,
+            categorySelected: this.state.visibleSansSerif,
+        })
+    }
 
-             />
-         )) :
+    showAll = (e) => {
+        e.preventDefault(e);
 
-<StyledScroll style={{overflow:'hidden'}}
-                            dataLength={fonts.length}
+        this.setState({ 
+            visibleFonts: this.state.selectedToDisplay.slice(0, 0),
+            results: 0,
+            hasMore: true,
+            selectedToDisplay: this.state.all,
+            categorySelected: this.state.all,
+        })
+    }
+
+
+    showRandom = (e) => {
+        e.preventDefault(e);
+
+        this.setState({ 
+            visibleFonts: this.state.selectedToDisplay.slice(0, 0),
+            results: 0,
+            hasMore: true, 
+            selectedToDisplay: this.state.randomFont,
+            categorySelected: this.state.randomFont,
+        })
+    }
+
+    fetchMoreData = () => {
+
+        if (this.state.visibleFonts.length === this.state.selectedToDisplay.length) {
+            
+            this.setState({ hasMore: false });
+            return;
+        }
+
+        setTimeout(() => {
+        this.setState(prevState =>({
+            results: prevState.results + 2,
+            visibleFonts:  this.state.selectedToDisplay.slice(0, prevState.results + 3),
+        }));
+        }, 500);
+    };
+
+    render(){
+        const {
+            error, 
+            hasMore, 
+            all,  
+            categorySelected, 
+            visibleFonts, 
+            visibleSerif, 
+            visibleSansSerif, 
+            fontFamilies, 
+            randomFont, 
+            fontsAmount
+        } = this.state;
+        
+        return (
+            <>
+                {        
+                    fontFamilies
+                }
+                <div>
+                    {  fontsAmount === null ?  <Loader /> : <Info info={fontsAmount} /> }
+                </div>
+                <p>{error}</p>
+
+                { 
+                
+                    fontsAmount !== null ? 
+                    <div> 
+                        <Button category={ categorySelected } whatToShow={randomFont} handleClickFn={this.showRandom}>random font</Button>
+                        <Button category={ categorySelected } whatToShow={all} handleClickFn={this.showAll}>all fonts</Button>
+                        <Button category={ categorySelected } whatToShow={visibleSerif} handleClickFn={this.showSerif}>serif only</Button>
+                        <Button category={ categorySelected } whatToShow={visibleSansSerif} handleClickFn={this.showSansSerif}>sans-serif only</Button>
+                    </div>
+                    : 
+                    null 
+                }
+                <div>
+                    {
+                        categorySelected === randomFont ?  randomFont.map(font => (
+                            <Font
+                                key={font.family}
+                                fontFamily={font.family}
+                                category={font.category}
+                            />
+                        )) :
+                        <StyledScroll   
+                            style={{overflow:'hidden'}}
+                            dataLength={visibleFonts.length}
                             next={this.fetchMoreData}
-                            hasMore={this.state.hasMore}
+                            hasMore={hasMore}
                             loader={<Loader />} 
                         >
-                                        {       
-               fonts.map(font => (
-                    <Font
-                        key={font.family}
-                        fontFamily={font.family}
-                        category={font.category}
-
-                    />
-                )) 
-            } 
+                            {       
+                                visibleFonts.map(font => (
+                                    <Font
+                                        key={font.family}
+                                        fontFamily={font.family}
+                                        category={font.category}
+                                    />
+                                )) 
+                            } 
                         </StyledScroll> 
-
-        }
-
-
-
-            
-
-            </div>
-        </>
-    );
-}
+                    }
+                </div>
+            </>
+        );
+    }
 }
 
 export default FontFinder;
