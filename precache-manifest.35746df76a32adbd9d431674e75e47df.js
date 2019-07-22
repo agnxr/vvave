a18c38243614c9c8819625c47778e3ec1c0da41a
Agnieszka Rakowska <ar21@o2.pl>
2019-07-22 14:18:57 +0200

    Updates

---
+++ precache-manifest.35746df76a32adbd9d431674e75e47df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2546d86266b97f98a214c56596962276",
+    "url": "/vvave/index.html"
+  },
+  {
+    "revision": "5443907f63385aad9786",
+    "url": "/vvave/static/js/2.7f664112.chunk.js"
+  },
+  {
+    "revision": "64bd3c52c09ca2faa92e",
+    "url": "/vvave/static/js/main.d08fc8bc.chunk.js"
+  },
+  {
+    "revision": "a92d38063f0ac01e9cbe",
+    "url": "/vvave/static/js/runtime~main.ff9edfbb.js"
+  },
+  {
+    "revision": "29d6f388c4404ae84a893e24caabe179",
+    "url": "/vvave/static/media/blue.29d6f388.mp4"
+  },
+  {
+    "revision": "d4e2d7afe4c1d47b06c0aa4e893a86c3",
+    "url": "/vvave/static/media/fallback.d4e2d7af.png"
+  },
+  {
+    "revision": "fe25f0c4f565c647f3f3b6ff3a8c8575",
+    "url": "/vvave/static/media/logo.fe25f0c4.svg"
+  },
+  {
+    "revision": "fc1f336a807e76598f47ccb05be851dd",
+    "url": "/vvave/static/media/pointer.fc1f336a.svg"
+  },
+  {
+    "revision": "b86c912c8232c7690fefbdc27c9f85aa",
+    "url": "/vvave/static/media/search.b86c912c.svg"
+  }
+]);
